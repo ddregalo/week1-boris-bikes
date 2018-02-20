6aea592e50a0a4ee6d0151f7d3aa11dc164034f8
danielcampos <ddregalo@gmail.com>
2018-02-20 19:58:56 +0000

    Add working? method to Bikes and modify both spec files (docking + bikes) to pass

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,9 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-  describe '.release_bike' do
-    it "expects docking station to release bike" do
-      expect(DockingStation).to respond_to(:release_bike)
-    end 
-  end
-end
+    it { should respond_to(:release_bike) }
+end 
