dcda78c99404aadeb891ca2f3689b8f08f2fbc9a
danielcampos <ddregalo@gmail.com>
2018-02-20 16:35:28 +0000

    Add bike class and bike spec test file

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,7 +3,7 @@ require 'docking_station'
 describe DockingStation do
   describe '.release_bike' do
     it "expects docking station to release bike" do
-      expect(DockingStation).to respond_to '.release_bike'
+      expect(DockingStation).to respond_to(:release_bike)
     end 
   end
 end
