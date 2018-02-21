baea3b17385b47bd6fdf4fd0a9ab5303cae0860d
danielcampos <ddregalo@gmail.com>
2018-02-21 16:40:09 +0000

    Write & pass test for working? method - expected to be true

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,6 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
   it { should respond_to(:release_bike) }
+  
 end
 
 describe 'release_bike' do
@@ -11,9 +12,6 @@ describe 'release_bike' do
   end
 end
 
-
-#  it "should return new instance of bike class" do
-#    station = DockingStation.new
-#    @bike = station.release_bike
-#    expect(station.release_bike).to eq(@bike)
-#  end
+describe Bike do
+  it { should be_working }
+end
