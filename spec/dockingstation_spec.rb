7fd4dfafe5c1d37fbb69821f7d4633e4e04708a3
danielcampos <ddregalo@gmail.com>
2018-02-21 15:45:32 +0000

    Add test to check if release_bike method returns new instance of bike

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,19 @@
 require 'docking_station'
 
 describe DockingStation do
-    it { should respond_to(:release_bike) }
-end 
+  it { should respond_to(:release_bike) }
+end
+
+describe 'release_bike' do
+  it "should return an instance of the Bike class" do 
+    station = DockingStation.new
+    expect(station.release_bike).to be_an_instance_of(Bike)  
+  end
+end
+
+
+#  it "should return new instance of bike class" do
+#    station = DockingStation.new
+#    @bike = station.release_bike
+#    expect(station.release_bike).to eq(@bike)
+#  end
