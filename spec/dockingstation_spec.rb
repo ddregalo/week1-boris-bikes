4623d09d8dc65a8287f6a2a2460fb25cf05f8cc0
Alfie Darko <me@alfiedarko.co.uk>
2018-02-22 12:55:45 +0000

    Added unit test for attr_reader and dock method

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,16 +2,28 @@ require 'docking_station'
 
 describe DockingStation do
   it { should respond_to(:release_bike) }
-  
 end
 
 describe 'release_bike' do
-  it "should return an instance of the Bike class" do 
+  it 'should return an instance of the Bike class' do
     station = DockingStation.new
-    expect(station.release_bike).to be_an_instance_of(Bike)  
+    expect(station.release_bike).to be_an_instance_of(Bike)
   end
 end
 
-describe Bike do
-  it { should be_working }
+describe 'dock' do
+  it 'should return bike' do
+    bike = Bike.new
+    docking_station = DockingStation.new
+    expect(docking_station.dock(bike)).to eq(bike)
+  end
+end
+
+describe 'attr_reader' do
+  it 'should return an instance of bike' do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    docking_station.dock(bike)
+    expect(docking_station.bike).to eq(bike)
+  end
 end
